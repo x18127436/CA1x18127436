3cba67b11363427b037d2fffa770955df38853ab
x18127436 <53307140+x18127436@users.noreply.github.com>
2020-08-02 06:59:06 -0700

    Add files via upload

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,7 +7,7 @@ class Game < ApplicationRecord
   mount_uploader :image, ImageUploader
   serialize :image, JSON # If you use SQLite, add this line
 
-  validates :title, :brand, :price, :publisher, presence: true
+  validates :title, :brand, :price, :publisher, :genre, :release_year, :image, presence: true
   validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum aloud. "}
   validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum aloud. "}
   validates :price, length: { maximum: 7 }
